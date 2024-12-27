8b6c7a5ab1767c279e15662338b84dd129de712c
Snackshell <solomonadonay1@gmail.com>
2024-12-27 12:07:59 +0300

    Update scheduler.js

--- src/utils/scheduler.js
+++ src/utils/scheduler.js
@@ -5,8 +5,8 @@ const EAST_AFRICAN_TIMEZONE = 'Africa/Addis_Ababa';
 
 const scheduleDaily = (taskFn) => {
   const rule = new schedule.RecurrenceRule();
-  rule.hour = 8;
-  rule.minute = 0;
+  rule.hour = 12;
+  rule.minute = 30;
   rule.tz = EAST_AFRICAN_TIMEZONE;
 
   return schedule.scheduleJob(rule, async () => {
@@ -18,5 +18,4 @@ const scheduleDaily = (taskFn) => {
     }
   });
 };
-
 module.exports = { scheduleDaily };
